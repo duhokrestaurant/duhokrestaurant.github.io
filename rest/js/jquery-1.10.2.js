3efd01b2aee77169f75b1ff4fe5a32a6cb825e58
duhokrestaurant <77174049+duhokrestaurant@users.noreply.github.com>
2021-01-09 13:45:38 +0300

    update

--- rest/js/jquery-1.10.2.js
+++ rest/js/jquery-1.10.2.js
@@ -4,12 +4,6 @@
  *
  * Includes Sizzle.js
  * http://sizzlejs.com/
- *
- * Copyright 2005, 2013 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2013-07-03T13:48Z
  */
 (function( window, undefined ) {
 
